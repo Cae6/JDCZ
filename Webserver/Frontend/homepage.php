061f9c8a9ebc605d67210df67182bbed4783a1c9
jam374 <134884537+jam374@users.noreply.github.com>
2025-03-19 13:14:06 -0400

    Update homepage.php

--- Webserver/Frontend/homepage.php
+++ Webserver/Frontend/homepage.php
@@ -5,7 +5,7 @@ session_start();
 // Check if the user is logged in
 if (!isset($_SESSION['username'])) {
     // Redirect to the login page if not logged in
-    header("Location: login.php");
+    header("Location: rabbitMQClient.php");
     exit();
 }
 ?>
