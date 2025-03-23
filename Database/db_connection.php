c656b8091c38bf09d54b1faa90c5784f86be7562
Zubaidah <72946117+zb64-web@users.noreply.github.com>
2025-03-23 22:58:15 +0000

    Update db_connection.php

--- Database/db_connection.php
+++ Database/db_connection.php
@@ -6,7 +6,7 @@ require_once('rabbitMQLib.inc');
 
 function connectToDatabase() {
     // 
-    $mydb = new mysqli('localhost', 'zb123', 'password', 'begindb');
+    $mydb = new mysqli('10.144.59.102', 'zb123', 'password', 'begindb');
     
     // Check connection
     if ($mydb->connect_error) {
